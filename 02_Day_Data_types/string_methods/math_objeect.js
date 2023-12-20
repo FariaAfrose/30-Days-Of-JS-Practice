36292832cf9204d0b18c75d588bafddfddc73f8d
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2023-12-20 12:11:01 +0600

    Add files via upload

---
+++ 02_Day_Data_types/string_methods/math_objeect.js
@@ -0,0 +1,29 @@
+const PI=Math.PI
+console.log(PI)
+console.log(Math.round(PI))
+console.log(Math.round(9.81))
+console.log(Math.floor(PI))// rounding down
+console.log(Math.ceil(PI))
+console.log(Math.min(-5,3,20,4,5,10))
+console.log(Math.max(-5,3,20,4,5,10))
+console.log(Math.random()) //random num between 0 -0.99999
+
+//creating num between o to 10
+console.log(Math.floor(Math.random()*11))
+//absolute value
+console.log(Math.abs(-10))
+//square root
+console.log(Math.sqrt(100))
+console.log(Math.sqrt(2))
+//power
+console.log(Math.pow(3,2))
+console.log(Math.E) //2.718
+//logarithm
+console.log(Math.log(2))
+console.log(Math.log(10))
+//trigonometry
+console.log(Math.sin(0))//0
+console.log(Math.sin(60))
+console.log(Math.cos(0))//1
+console.log(Math.cos(60))
+
---
+++ 02_Day_Data_types/string_methods/string_concatanation.js
@@ -0,0 +1,14 @@
+let space =' '
+let firstName='Asabeneh'
+let lastName='Yetayeh'
+let country='Finland'
+let city='Helsinki'
+let language='JavaScript'
+let job='teacher'
+
+//concatanating
+let fullName= firstName+ space+lastName
+console.log(fullName)
+
+let personInfoOne= fullName + '. I am ' +age  + '. I live in ' + country
+console.log(personInfoOne)
