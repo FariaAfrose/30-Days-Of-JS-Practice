d01b147df6b046922b05dcba38f0c04e99095034
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2023-12-20 12:07:12 +0600

    Delete string_methods directory

--- string_methods/number_data_type.js
+++
@@ -1,9 +0,0 @@
-let age =35
-const gravity=9.81
-let mass=72
-
-
-// const PI=3.14
- const boilingPoint=100
- const bodyTemp=37
- console.log(age +","+ gravity +","+ mass +","+ PI +","+ boilingPoint +","+ bodyTemp)
--- string_methods/string_data_type.js
+++
@@ -1,7 +0,0 @@
-// let space =' '
-// let firstName='Asabeneh'
-// let lastName='Yetayeh'
-// let country='Finland'
-// let city='Helsinki'
-// let language='JavaScript'
-// let job='teacher'
