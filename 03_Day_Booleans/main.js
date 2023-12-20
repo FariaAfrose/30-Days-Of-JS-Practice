4f3b1dad44bcd5c94012eb79fd15aa785d47d91a
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2023-12-20 12:53:59 +0600

    Add files via upload

---
+++ 03_Day_Booleans/main.js
@@ -0,0 +1,13 @@
+let firstName
+console.log(firstName)
+let empty =null
+console.log(empty)
+//getting hours
+const now= new Date()
+console.log(now.getHours())
+//getting minutes
+const min= new Date()
+console.log(now.getMinutes())
+//getting seconds
+const nosecw= new Date()
+console.log(now.getSeconds())
