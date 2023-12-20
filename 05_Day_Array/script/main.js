7607f281fc4bb138f60f1d5a74d4fd218cb09254
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2023-12-20 14:39:09 +0600

    Add files via upload

---
+++ 05_Day_Array/script/main.js
@@ -0,0 +1,20 @@
+console.log(countries)
+alert ('Open the browser console whenever you work on JavaScript')
+alert ('Open the  console and if the countrics has been loaded')
+//using split
+let js= 'JAVASCRIPT'
+const charInIs=js.split('')
+console.log(charInIs)
+//accessing array items using index 
+ const items=['one','two','three','four']
+ let firstItem= items[0]
+ console.log(firstItem)
+ let lastItem= items.length -1
+ lastItem=items[lastItem]
+ console.log(lastItem)
+ 
+ //concatenating array
+ const firstList =[1,2,3]
+ const secondList=[4,5,6]
+ const thirdList=firstList.concat(secondList)
+ console.log(thirdList)
