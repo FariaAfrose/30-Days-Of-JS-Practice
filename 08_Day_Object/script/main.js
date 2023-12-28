95b535c88a4094bc70be33b8a48029b45c8f8bfd
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2023-12-28 16:32:21 +0600

    Update main.js

--- 08_Day_Object/script/main.js
+++ 08_Day_Object/script/main.js
@@ -8,7 +8,7 @@ dog.name="loye"
 dog.age=20
 dog.legs=2
 dog.color='brown'
-dog.bard=function(){
+dog.bark=function(){
    return 'woolf-woolf'
 }
 //geting
@@ -16,12 +16,12 @@ const dogName=dog.name;
 const dogAge=dog.age;
 const dogLegs=dog.legs;
 const dogColor=dog.color;
-const dogBark=dog.bark;
+const dogBark=dog.bark();
 //seting new property
  dog.gender='male'
  dog.info= function (){
-return `Dog name is ${this.name} age is  ${this.age} , color is ${this.color}$ and sound ..{this.bark}`
+return `Dog name is ${this.name}, age is  ${this.age} , color is ${this.color}  and sound ..  ${this.bark()}`
 }
 console.log(dogName ,dogAge,dogLegs,dogColor)
-const info=dogInfo()
+const info=dog.info()
 console.log(info)
