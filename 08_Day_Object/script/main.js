d0428716104d692c97022bbb8dfb181ff8a98203
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2023-12-28 16:28:19 +0600

    Add files via upload

---
+++ 08_Day_Object/script/main.js
@@ -0,0 +1,27 @@
+console.log(countries)
+// creating and empty object
+const dog={}
+//creating object 
+console.log(dog)
+//adding 
+dog.name="loye"
+dog.age=20
+dog.legs=2
+dog.color='brown'
+dog.bard=function(){
+   return 'woolf-woolf'
+}
+//geting
+const dogName=dog.name;
+const dogAge=dog.age;
+const dogLegs=dog.legs;
+const dogColor=dog.color;
+const dogBark=dog.bark;
+//seting new property
+ dog.gender='male'
+ dog.info= function (){
+return `Dog name is ${this.name} age is  ${this.age} , color is ${this.color}$ and sound ..{this.bark}`
+}
+console.log(dogName ,dogAge,dogLegs,dogColor)
+const info=dogInfo()
+console.log(info)
