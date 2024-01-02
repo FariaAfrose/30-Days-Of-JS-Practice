8b76cd3f476f1cc7c9ebe43820911f1de9e77fb7
FariaAfrose <faria.afrose@strativ.se>
2024-01-02 17:26:17 +0600

    new

---
+++ 02_Day_Data_types/string_methods/primitive_data_type.js
@@ -0,0 +1,13 @@
+let word ='JavaScript'
+
+let numOne=3
+let numTwo=3
+console.log(numOne==numTwo)
+
+let js='JavaScript'
+let py='Python'
+console.log(js==py)
+
+let lightOn=true
+let lightOff=false
+console.log(lightOn ==lightOff)
---
+++ 09_Day_higher_order_fun/data/country.js
@@ -0,0 +1,18 @@
+const countries = [
+    {
+      name: 'Afghanistan',
+      capital: 'Kabul',
+      languages: ['Pashto', 'Uzbek', 'Turkmen'],
+      population: 27657145,
+      flag: 'https://restcountries.eu/data/afg.svg',
+      currency: 'Afghan afghani'
+    },
+    {
+      name: 'Åland Islands',
+      capital: 'Mariehamn',
+      languages: ['Swedish'],
+      population: 28875,
+      flag: 'https://restcountries.eu/data/ala.svg',
+      currency: 'Euro'
+    },
+]
