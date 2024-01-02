f28a07579ad8f56d94fd41f0dfabaa4f2f4ddee6
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2024-01-02 17:28:59 +0600

    Add files via upload

---
+++ 10_Day_sets_and_map/script/main.js
@@ -0,0 +1,33 @@
+//const a = [4, 5, 8, 9]
+// const b = [3, 4, 5, 7]
+// const countries = ['Finland', 'Sweden', 'Norway']
+// create an empty set
+const a=[4,5,8,9]
+const b=[3,4,5,7]
+const emptySet=new Set()
+console.log(emptySet)
+// console.log(b)
+// const newSetB=new Set(b)
+
+// console.log(newSetB)
+// Create a set containing 0 to 10 using loop
+const createSet=new Set()
+
+for(let i=0; i<=10;i++){
+    createSet.add(i)
+}
+console.log(createSet)
+
+// Remove an element from a set
+// Clear a set
+// Create a set of 5 string elements from array
+// Create a map of countries and number of characters of a country
+
+//Exercises:Level 2
+// Find a union b
+// Find a intersection b
+// Find a with b
+
+//How many languages are there in the countries object file.
+
+// Use the countries data to find the 10 most spoken languages:
