aecc055ead5ed030acab7c667734083f0899e82e
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2024-01-03 17:32:33 +0600

    Update main.js

--- 10_Day_sets_and_map/script/main.js
+++ 10_Day_sets_and_map/script/main.js
@@ -1,41 +1,77 @@
-//const a = [4, 5, 8, 9]
-// const b = [3, 4, 5, 7]
-// const countries = ['Finland', 'Sweden', 'Norway']
-// create an empty set
-const a=[4,5,8,9]
-const b=[3,4,5,7]
 
+//1. create an empty set
+const a=[4,5,8,9]
+const b=[3,4,4,5,7]
 const emptySet=new Set()
 console.log(emptySet)
 // console.log(b)
-// const newSetB=new Set(b)
+const newSetB=new Set(b)
+console.log(newSetB)
 
-// console.log(newSetB)
-// Create a set containing 0 to 10 using loop
+//2. Create a set containing 0 to 10 using loop
 const createSet=new Set()
-
 for(let i=0; i<=10;i++){
-    createSet.add(i)
-}
+ createSet.add(i)}
 console.log(createSet)
 
-// Remove an element from a set
+//3. Remove an element from a set
   createSet.delete(4)
   console.log(createSet)
-// Clear a set
+
+//4. Clear a set
 createSet.clear()
 console.log(createSet)
-// Create a set of 5 string elements from array
+
+//5. Create a set of 5 string elements from array
 const creatingArray=["a","b","c","d","d","e","f"]
 const creatingSet= new Set(creatingArray.slice(0,5))
 console.log(creatingSet)
-// Create a map of countries and number of characters of a country
 
-//Exercises:Level 2
-// Find a union b
-// Find a intersection b
+//6.empty Map (Map- built-in object)
+const map=new Map()
+console.log(map)
+
+//7. Create a map of countries and number of characters of a country
+countrys = ['Finland', 'Helsinki','Sweden', 'Stockholm' ,'Norway', 'Oslo']
+//map(array method)
+const countriesMap= new  Map(countrys.map(country=>[country,country.length]))
+console.log(countriesMap)
+//8.Exercises:Level 2
+//Find a union b
+const unionSet= new Set([...a,...b])
+console.log(Array.from (unionSet))
+//Find a intersection b
+const intersectionSet=new Set(a.filter(a=> b.includes(a)))
+console.log(Array.from (intersectionSet))
 // Find a with b
-
+const cmmonInAB=a.filter(a=> b.includes(a))
+console.log(Array.from (cmmonInAB))
 //How many languages are there in the countries object file.
 
-// Use the countries data to find the 10 most spoken languages:
+function countLanguage(countries){
+  const languageSet=new Set()
+
+  countries.forEach(country => {
+    country.languages.forEach(language=>{
+    languageSet.add(language)
+    })
+  });
+  return languageSet.size
+}
+const numOfLang=countLanguage(countries)
+console.log(numOfLang)
+// Use the countries data to find the 10 most spoken languages
+function spokenLanguage(countries,count){
+  const languageCount={}
+
+  countries.forEach(country => {
+    country.languages.forEach(langauge=>{
+     languageCount[langauge]= (languageCount[langauge] || 0)+1
+    })
+  });
+  const languageArray =Object.entries(languageCount).map(([langauge,count])=>({[langauge]:count}))
+  const sortedLang=languageArray.sort((a,b)=>Object.values(b)[0]-Object.values(a)[0])
+  return sortedLang.slice(0,count)
+  }  
+  console.log(spokenLanguage(countries,10))
+  console.log(spokenLanguage(countries,3))
