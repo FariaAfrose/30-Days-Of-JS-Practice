1f5c0ba93a4be90c67de9c35417bbec9a02bbaf9
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2024-01-18 15:52:13 +0600

    Update main.js

--- 25_Day_data_visualization1/script/main.js
+++ 25_Day_data_visualization1/script/main.js
@@ -20,8 +20,17 @@ return ten
 
 function Language(){
     feedback.innerHTML=`10 Most Spoken languages in the world`
-   
+   const languageCount={}
+   countries_data.forEach(country=>{
+    country.languages.forEach(language=>{
+        languageCount[language]=(languageCount[language] ||0)+1
+    })
+   })
+   const sortLanguage=Object.entries(languageCount).sort((a,b)=>b[1]-a[1])
+const top10Language=sortLanguage.slice(0,10)
+return top10Language
 }
+
 const populatedCountries=Population(countries_data)
 const nameAndPopulation = populatedCountries.map(country => ({ name: country.name, population: country.population }));
 const countryDiv=document.querySelector('.container')
@@ -61,7 +70,39 @@ progressbar.style.width=`${widthPercentage}%`
     countryDiv.innerHTML= ""
     countryDiv.appendChild(divPairContainer)
 });
+language.addEventListener('click',function(){
+    const topLanguage=Language(countries_data)
+    const divPairContainer=document.createElement('div')
+    
+    divPairContainer.classList = 'container';
+    topLanguage.forEach(([language ,count]) => {
+        const divPair=document.createElement('div')
+        divPair.classList='div-pair'
+
+        const languageName=document.createElement('div')
+        languageName.classList.add('countryName')
+        languageName.textContent = language;
+
+        const progressbarContainer = document.createElement('div');
+        progressbarContainer.classList = 'progressbarContainer';
+        const progressbar = document.createElement('div');
+        progressbar.classList = 'progress-bar';
+
+        const languageCount=document.createElement('div')
+        languageCount.classList.add('countryPopulation')
+        languageCount.textContent =count
+const widthPercentage=(count/topLanguage[0][1])*100
+progressbar.style.width=`${widthPercentage}%`
+
+        divPair.appendChild(languageName);
+        progressbarContainer.appendChild(progressbar);
+        divPair.appendChild(progressbarContainer);
+        divPair.appendChild(languageCount);
+
+        divPairContainer.appendChild(divPair)
+})
 
 
-
- 
+countryDiv.innerHTML= ""
+countryDiv.appendChild(divPairContainer)
+});
